a431415606e2c05955d50073179da894d6e47896
ViswakGgautham <146814448+ViswakGgautham@users.noreply.github.com>
2023-10-05 15:33:17 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,28 +1,44 @@
 import mysql.connector as mysql
 con = mysql.connect(host='localhost',password='password',database='feedback',user='root')
 cursor=con.cursor()
-def insert(box,text):
-    querry=f'insert into feedback1 values("{box}","{text}");'
+def insert(class1,box,text):
+    querry=f'insert into feedback1 values("{box}","{text}","{class1}");'
     cursor.execute(querry)
     con.commit()
-def count():
-    querry='select count(*) from feedback1 where option="green"'
+def count(id):
+    querry=f'select count(*) from feedback1 where option="green" and classID="{id}"'
     cursor.execute(querry)
     green=cursor.fetchall()[0][0]
-    querry = 'select count(*) from feedback1 where option="red"'
+    querry = f'select count(*) from feedback1 where option="red" and classID="{id}"'
     cursor.execute(querry)
     red=cursor.fetchall()[0][0]
-    querry = 'select count(*) from feedback1 where option="amber"'
+    querry = f'select count(*) from feedback1 where option="amber" and classID="{id}"'
     cursor.execute(querry)
     amber = cursor.fetchall()[0][0]
     t=(green,amber,red)
     return t
-def comments():
+def comments(id):
     l=[]
-    querry='select description from feedback1;'
+    querry=f'select description from feedback1 where classID="{id}";'
     cursor.execute(querry)
     data=cursor.fetchall()
     for i in data:
         for j in i:
             l.append(j)
     return l
+def insert_class(id,no,year):
+    query=f'insert into classes values("{id}",{no},{year})'
+    cursor.execute(query)
+    con.commit()
+def classnames():
+    l=[]
+    querry='select class from classes'
+    cursor.execute(querry)
+    data=cursor.fetchall()
+    for i in data:
+        for j in i:
+            l.append(j)
+    return l
+
+
+
